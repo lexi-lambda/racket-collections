d35875f4fa29340769f222ca8518107d4dc68700
Alexis King <lexi.lambda@gmail.com>
2016-01-03 18:59:09 -0800

    Move docs into the "Data Libraries" category

--- data/info.rkt
+++ data/info.rkt
@@ -1,6 +1,6 @@
 #lang info
 
-(define scribblings '(["scribblings/collections.scrbl" (multi-page)]))
+(define scribblings '(["scribblings/collections.scrbl" (multi-page) ("Data Libraries")]))
 
 (define cover-omit-paths
   '("scribblings"))
