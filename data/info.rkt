e808b58e5cea587845596c1e3440e4ec0f67754c
Alexis King <lexi.lambda@gmail.com>
2016-01-09 12:06:33 -0800

    Revert "Move docs into the "Data Libraries" category"
    While this only causes a warning on earlier Racket versions, not an
    error, it causes building the docs to fail. I'll reconsider this once
    the pkg-build server supports this feature, but until then, it isn't
    really worth it.
    
    This reverts commit d35875f4fa29340769f222ca8518107d4dc68700.

--- data/info.rkt
+++ data/info.rkt
@@ -1,6 +1,6 @@
 #lang info
 
-(define scribblings '(["scribblings/collections.scrbl" (multi-page) ("Data Libraries")]))
+(define scribblings '(["scribblings/collections.scrbl" (multi-page)]))
 
 (define cover-omit-paths
   '("scribblings"))
